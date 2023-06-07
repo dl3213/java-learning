fdd4c16c92ce0f698443ff85194c37eab508173f
dl3213 <dl3213@192.168.0.101>
2023-06-07 23:27:14 +0800

    复习：希尔排序

---
+++ sibyl-util/src/main/java/me/sibyl/structure/test/sort/ShellSort.java
@@ -0,0 +1,36 @@
+package me.sibyl.structure.test.sort;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * @author dyingleaf3213
+ * @Classname ShellSort
+ * @Description 希尔排序，属于插入排序
+ * @Create 2023/06/05 22:35
+ */
+
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] data = SortUtil.getData_100000();
+        long start = System.currentTimeMillis();
+
+        int gap = data.length / 2; // 分组
+        int temp = 0, j = 0;
+        while (gap > 0) {
+            for (int i = 0; i < data.length; i++) { // 局部排序
+                temp = data[i];
+                j = i - gap;
+                while (j >= 0 && temp < data[j]) {
+                    data[j + gap] = data[j];
+                    j -= gap;
+                }
+                data[j + gap] = temp;
+            }
+            gap /= 2;// 每组再分组
+        }
+
+        System.err.println(System.currentTimeMillis() - start); // 36
+        System.err.println(Arrays.stream(data).boxed().collect(Collectors.toList()));
+    }
+}
